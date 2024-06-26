8e0fc8bb5ec782c90479b1243f540c2d7c58d8f4
Gediminas Bukauskas <gbukauskas@gmail.com>
2024-06-26 10:23:46 +0300

    Pager is ready

--- BlazorApp_1/obj/Debug/net8.0/BlazorApp_1.AssemblyInfo.cs
+++ BlazorApp_1/obj/Debug/net8.0/BlazorApp_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c9ee267032490891815a6a46ca00439297bdb5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a74468002032870d0c749648f4daaa9349582dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
